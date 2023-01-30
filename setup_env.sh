0c870ca1f2a3742c23a12b4e651998c282dbd6d9
MrArtemSid <artem.sid.key@gmail.com>
2023-01-30 18:38:15 +0400

    IP: Add more phpmyadmin stuff to get it working

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman --noconfirm -Sy apache php-apache mysql phpmyadmin
+sudo pacman --noconfirm -Sy apache php-apache mysql phpmyadmin php-mcrypt
 sudo mysqld --initialize --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 sudo systemctl start mysqld
 sudo mysql_secure_installation
